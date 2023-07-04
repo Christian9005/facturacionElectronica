0c0417e043974727572ee79c46c75d92abc3b082
Christian Albarracin <christianalex-071@hotmail.com>
2023-07-04 10:33:53 -0500

    first commit

---
+++ billing.php
@@ -0,0 +1,7 @@
+<?php
+error_reporting(E_ALL);
+ini_set('ignore_repeated_errors', true);
+ini_set('display_errors', true);
+ini_set('log_errors', true);
+ini_set('error_log', __DIR__ . '\php-error.log');
+error_log('App Started');
